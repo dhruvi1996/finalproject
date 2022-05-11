11c79ec739be6a3473b2f2ff8c6522c1b006edcc
dhruvi1996 <ddm8@njit.edu>
2022-05-11 13:41:32 -0400

    declutter and did logging and test again

--- app/__init__.py
+++ app/__init__.py
@@ -1,26 +1,26 @@
 """A simple flask web app"""
-import logging
 import os
-from logging.handlers import RotatingFileHandler
 
 import flask_login
 from flask import Flask
 from flask_bootstrap import Bootstrap5
+from flask_cors import CORS
+from flask_mail import Mail
 from flask_wtf.csrf import CSRFProtect
 
 from app.auth import auth
 from app.cli import create_database, create_log_folder
 from app.context_processors import utility_text_processors
+from app.db import database
 from app.db import db
 from app.db.models import User
 from app.error_handlers import error_handlers
 from app.logging_config import log_con, LOGGING_CONFIG
 from app.simple_pages import simple_pages
 from app.transactions import transactions
-from app.db import database
-from flask_cors import CORS
-login_manager = flask_login.LoginManager()
 
+mail = Mail()
+login_manager = flask_login.LoginManager()
 
 def create_app():
     """Create and configure an instance of the Flask application."""
@@ -31,10 +31,12 @@ def create_app():
         app.config.from_object("app.config.DevelopmentConfig")
     elif os.environ.get("FLASK_ENV") == "testing":
         app.config.from_object("app.config.TestingConfig")
+    app.mail = Mail(app)
 
     # https://flask-login.readthedocs.io/en/latest/  <-login manager
     login_manager.init_app(app)
     login_manager.login_view = "auth.login"
+
     # Needed for CSRF protection of form submissions and WTF Forms
     # https://wtforms.readthedocs.io/en/3.0.x/
     csrf = CSRFProtect(app)
@@ -48,13 +50,13 @@ def create_app():
     app.register_blueprint(log_con)
     app.register_blueprint(error_handlers)
     app.register_blueprint(transactions)
-    app.register_blueprint(map)
     app.context_processor(utility_text_processors)
     # add command function to cli commands
     app.cli.add_command(create_database)
+    app.cli.add_command(create_log_folder)
     db.init_app(app)
     api_v1_cors_config = {
-    "methods": ["OPTIONS", "GET", "POST"],
+        "methods": ["OPTIONS", "GET", "POST"],
     }
     CORS(app, resources={"/api/*": api_v1_cors_config})
     # Run once at startup:
--- app/db/models/__init__.py
+++ app/db/models/__init__.py
@@ -1,57 +1,29 @@
 from datetime import datetime
-
-from sqlalchemy import Integer, ForeignKey
-from sqlalchemy.orm import relationship
+from flask_login import UserMixin
+from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy_serializer import SerializerMixin
 from werkzeug.security import check_password_hash, generate_password_hash
 from app.db import db
-from flask_login import UserMixin
-from sqlalchemy_serializer import SerializerMixin
 
-transaction_user = db.Table('transaction_user', db.Model.metadata,
-                     db.Column('user_id', db.Integer, db.ForeignKey('users.id')),
-                     db.Column('transaction_id', db.Integer, db.ForeignKey('transactions.id'))
-                     )
+Base = declarative_base()
 
-
-class Transaction(db.Model, SerializerMixin):
+class Transaction(db.Model,SerializerMixin):
     __tablename__ = 'transactions'
     id = db.Column(db.Integer, primary_key=True)
-    type = db.Column(db.String(300), nullable=True, unique=False)
-    amount = db.Column(db.Integer, nullable=False, unique=False)
+    AMOUNT = db.Column(db.Float(), nullable=False, unique=False)
+    TYPE = db.Column(db.String(6), nullable=False, unique=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     user = relationship("User", back_populates="transactions", uselist=False)
 
-    def __init__(self, amount, type):
-        self.amount = amount
-        self.type = type
-
-
-class Location(db.Model, SerializerMixin):
-    __tablename__ = 'locations'
-    serialize_only = ('title', 'longitude', 'latitude')
-
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(300), nullable=True, unique=False)
-    longitude = db.Column(db.String(300), nullable=True, unique=False)
-    latitude = db.Column(db.String(300), nullable=True, unique=False)
-    population = db.Column(db.Integer, nullable=True, unique=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    user = relationship("User", back_populates="locations", uselist=False)
-
-    def __init__(self, title, longitude, latitude, population):
-        self.title = title
-        self.longitude = longitude
-        self.latitude = latitude
-        self.population = population
+    def __init__(self, AMOUNT, TYPE):
+        self.AMOUNT = AMOUNT
+        self.TYPE = TYPE
 
     def serialize(self):
         return {
-            'title': self.title,
-            'long': self.longitude,
-            'lat': self.latitude,
-            'population': self.population,
+            'AMOUNT': self.AMOUNT,
+            'TYPE': self.TYPE,
         }
-
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
@@ -63,16 +35,14 @@ class User(UserMixin, db.Model):
     registered_on = db.Column('registered_on', db.DateTime)
     active = db.Column('is_active', db.Boolean(), nullable=False, server_default='1')
     is_admin = db.Column('is_admin', db.Boolean(), nullable=False, server_default='0')
+    balance = db.Column(db.Float, nullable=True, unique=False, default="0.00")
     transactions = db.relationship("Transaction", back_populates="user", cascade="all, delete")
-    locations = db.relationship("Location", back_populates="user", cascade="all, delete")
 
-    # `roles` and `groups` are reserved words that *must* be defined
-    # on the `User` model to use group- or role-based authorization.
-
-    def __init__(self, email, password):
+    def __init__(self, email, password, is_admin):
         self.email = email
         self.password = password
         self.registered_on = datetime.utcnow()
+        self.is_admin = is_admin
 
     def is_authenticated(self):
         return True
